0aacab0480d7f2ff4f5c8c48f49db57d014b8a10
Matheus Eduardo <88885147+matheusevs@users.noreply.github.com>
2024-09-02 09:20:58 -0300

    feat: Comparando e modificando valores

--- main.js
+++ main.js
@@ -50,11 +50,13 @@ function mostrarItem(){
                 <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
                     <div>
                         <input type="checkbox" class="is-clickable" />
-                        <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+                        <input type="text" class="is-size-5" value="${elemento.valor}" ${index !== Number(itemAEditar) ? 'disabled' : ''}></input>
                     </div>
                     <div>
-                        <button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>
-                        <i class="fa-regular is-clickable fa-pen-to-square editar"></i>
+                        ${index === Number(itemAEditar)
+                            ? '<button onclick="salvarEdicao()"><i class="fa-regular fa-floppy-disk is-clickable"></i></button>'
+                            : '<i class="fa-regular is-clickable fa-pen-to-square editar"></i>'
+                        }
                         <i class="fa-solid fa-trash is-clickable deletar"></i>
                     </div>
                 </li>`;
